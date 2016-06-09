7dc44b4cd97ca3aa7899b06b8092cf5e4e01e23a
JLamim Projetos Web & Mobile <dev@jlamim.com.br>
2016-06-09 12:18:07 -0300

    Arquivos completos

---
+++ README.md
@@ -0,0 +1,8 @@
+# Criando gráficos com PHPlot
+
+Arquivos do tutorial ensinando a utilizar a biblioteca de criação de gráficos PHPlot integrada ao CodeIgniter
+
+Veja o tutorial em:
+
+http://www.universidadecodeigniter.com.br/criando-graficos-com-phplot
+
---
+++ application/controllers/Base.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Base extends CI_Controller {
+
+	public function Index(){
+		// Carregamos a library PHPlot
+		$this->load->library('phplot');
+
+		//Definindo os dados do gráfico
+		$dados = array(
+			array('a',3),
+			array('b',5),
+			array('c',7),
+			array('d',8),
+			array('e',2),
+			array('f',6),
+			array('g',7)
+		);
+		$this->phplot->SetDataValues($dados);
+
+		//Imprimindo o gráfico na tela
+		$this->phplot->DrawGraph();
+	}
+
+	public function Comtitulos()
+	{
+		// Carregamos a library PHPlot
+		$this->load->library('phplot');
+
+		//Define os títulos
+		$this->phplot->SetTitle("Titulo do Grafico");
+		$this->phplot->SetXTitle('Eixo X');
+		$this->phplot->SetYTitle('Eixo Y');
+
+		//Define os valores para geração do gráfico
+		$dados = array(
+			array('a',3,4,2),
+			array('b',5,9,1),
+			array('c',7,2,6),
+			array('d',8,1,4),
+			array('e',2,8,6),
+			array('f',6,4,5),
+			array('g',7,2,3)
+		);
+		$this->phplot->SetDataValues($dados);
+
+		//Imprime o gráfico na tela
+		$this->phplot->DrawGraph();
+	}
+
+}
